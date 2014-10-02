ae1ddb65da2efd6310fae2d05e551d55affdccb4
Mike Skull <mjskull@gmail.com>
2014-10-02 17:10:46 +0100

    Updated to Alpha 7, added base blasting charge into this mod as removed from core game

---
+++ MoreBlastingCharges/MoreBlastingChargesDLL/MoreBlastingChargesDLL/CompGlowerX.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Verse;
+public class CompGlowerX : CompGlower
+{
+    private bool glowOnInt;
+    public bool LitX
+    {
+        get
+        {
+            return this.glowOnInt;
+        }
+        set
+        {
+            if (this.glowOnInt == value)
+            {
+                return;
+            }
+            this.glowOnInt = value;
+            if (!value)
+            {
+                Find.MapDrawer.MapChanged(this.parent.Position, MapChangeType.Things);
+                Find.GlowGrid.DeRegisterGlower(this);
+                return;
+            }
+            Find.MapDrawer.MapChanged(this.parent.Position, MapChangeType.Things);
+            Find.GlowGrid.RegisterGlower(this);
+        }
+    }
+    public int RadiusIntCeilingX
+    {
+        get
+        {
+            return (int)Math.Ceiling((double)this.props.glowRadius);
+        }
+    }
+    public override void PostSpawnSetup()
+    {
+        if (this.Lit)
+        {
+            Find.GlowGrid.RegisterGlower(this);
+        }
+        this.Lit = true;
+    }
+    public override void ReceiveCompSignal(string signal)
+    {
+    }
+    public override void PostExposeData()
+    {
+        Scribe_Values.LookValue<bool>(ref this.glowOnInt, "glowOn", false, false);
+    }
+    public override void PostDestroy(DestroyMode mode = DestroyMode.Vanish)
+    {
+        base.PostDestroy(mode);
+        this.Lit = false;
+    }
+}
