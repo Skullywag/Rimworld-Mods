7728d9457e96cb4808c39f2fb6f3da0878a8374c
Mike Skull <mjskull@gmail.com>
2014-10-09 21:59:00 +0100

    added railgun

---
+++ MoreWeapons/MoreWeaponsDLL/MoreWeaponsDLL/Projectile_RailGunBullet.cs
@@ -0,0 +1,50 @@
+﻿using RimWorld;
+using System;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+namespace MoreWeapons
+{
+    public class Projectile_RailGunBullet : Bullet
+    {
+        private DamageTypeDef dmgdef;
+        public override void SpawnSetup()
+        {
+            base.SpawnSetup();
+            dmgdef = DefDatabase<DamageTypeDef>.GetNamed("Vaporized", true);
+            MoteMaker.ThrowFlash(base.Position, "SparkFlash", 20f);
+        }
+        public override void Tick()
+        {
+            base.Tick();
+            MoteMaker.TryThrowMicroSparks(base.Position.ToVector3Shifted());
+        }
+
+        protected override void Impact(Thing hitThing)
+        {
+            if (hitThing != null)
+            {
+                int damageAmountBase = this.def.projectile.damageAmountBase;
+                DamageInfo damageInfo = new DamageInfo(this.dmgdef, damageAmountBase, this.launcher, this.ExactRotation.eulerAngles.y, null, null);
+                hitThing.TakeDamage(damageInfo);
+                Pawn pawn = hitThing as Pawn;
+                if (pawn != null)
+                {
+                    hitThing.TakeDamage(new DamageInfo(dmgdef, 0, this.launcher, null, null));
+                    MoteMaker.ThrowLightningGlow(base.Position.ToVector3Shifted(), 1F);
+                    MoteMaker.TryThrowMicroSparks(base.Position.ToVector3Shifted());
+                    if (pawn.IsColonist)
+                    {
+                        Messages.Message(pawn.Name.first + " " + " " + pawn.Name.last + " was vaporized.", MessageSound.Negative);
+                    }
+                    pawn.Destroy();
+                }
+            }
+            else
+            {
+                MoteMaker.MakeShotHitDirt(this.ExactPosition);
+            }
+            this.Destroy();
+        }
+    }
+}
