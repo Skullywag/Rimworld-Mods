d38e4090f797d15f1d80f1ed5b3b92dbc577bad4
Mike Skull <mjskull@gmail.com>
2014-10-27 23:27:34 +0000

    added simpletorch mod

---
+++ SimpleTorch/SimpleTorchDLL/SimpleTorch/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MorePower")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MorePower")]
+[assembly: AssemblyCopyright("Copyright ©  2014 (skullywag)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a8e081b6-8768-4fe9-817f-f0ee42cf13ef")]
+
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
