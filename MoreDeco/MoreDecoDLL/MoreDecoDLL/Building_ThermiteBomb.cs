901a1d8b144e35139448fd3071e0e426555e13fe
Mike Skull <mjskull@gmail.com>
2014-11-04 21:25:30 +0000

    added plantable berry bushes and updated thermite bomb

--- MoreDeco/MoreDecoDLL/MoreDecoDLL/Building_ThermiteBomb.cs
+++ MoreDeco/MoreDecoDLL/MoreDecoDLL/Building_ThermiteBomb.cs
@@ -47,6 +47,18 @@ namespace MoreDeco
             explosionInfo.dinfo = new DamageInfo(dmgdef, 100, this, new BodyPartDamageInfo?(value), null);
             explosionInfo.dinfo = new DamageInfo(dmgdef, 100, this, new BodyPartDamageInfo?(value), null);
             MoteMaker.TryThrowMicroSparks(Position.ToVector3Shifted());
+            if(Position.GetRoof() != null)
+            {
+                if (Find.RoofGrid.RoofDefAt(Position).isThickRoof == true)
+                {
+                    RoofDef roofType = DefDatabase<RoofDef>.GetNamed("RoofRockThin");
+                    Find.RoofGrid.SetRoof(Position, roofType);
+                }
+                else
+                {
+                    Find.RoofGrid.SetRoof(Position, null);
+                }    
+            }
             foreach (IntVec3 current in GenAdj.AdjacentSquares8Way(this))
             {
                 if (current.GetRoof() != null)
@@ -54,11 +66,11 @@ namespace MoreDeco
                     if (Find.RoofGrid.RoofDefAt(current).isThickRoof == true)
                     {
                         RoofDef roofType = DefDatabase<RoofDef>.GetNamed("RoofRockThin");
-                        Find.RoofGrid.SetRoof(Position, roofType);
+                        Find.RoofGrid.SetRoof(current, roofType);
                     }
                     else
                     {
-                        Find.RoofGrid.SetRoof(Position, null);
+                        Find.RoofGrid.SetRoof(current, null);
                     }
                 }
             }
