93916cf1ee1bbcd1498bd1a6b4b5545d9829938e
Mike Skull <mjskull@gmail.com>
2014-12-17 21:50:37 +0000

    Split more weapons into individual mods

---
+++ FireExtinguisher/FireExtinguisherDLL/FireExtinguisherDLL/Projectile_FireExtinguisher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using Verse;
+using Verse.Sound;
+namespace RimWorld
+{
+    public class Projectile_FireExtinguisher : Bullet
+    {
+        public override void SpawnSetup()
+        {
+            base.SpawnSetup();
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+        }
+
+        protected override void Impact(Thing hitThing)
+        {
+            base.Impact(hitThing);
+            MoteMaker.TryThrowSmoke(base.Position.ToVector3Shifted(), 1f);
+            foreach (IntVec3 current in GenAdj.AdjacentCells8WayAndInside(Position))
+            {
+                List<Thing> list = Find.ThingGrid.ThingsListAt(current);
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i].def.eType == EntityType.Fire)
+                    {
+                        list[i].TakeDamage(new DamageInfo(DamageTypeDefOf.Extinguish, 2, this, null, null));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FlareGun/FlareGunDLL/FlareGunDLL/Projectile_FlareGunBullet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+namespace RimWorld
+{
+    public class Projectile_FlareGunBullet : Bullet
+    {
+        private DamageTypeDef dmgdef;
+
+        public override void SpawnSetup()
+        {
+            base.SpawnSetup();
+            MoteMaker.TryThrowSmoke(base.Position.ToVector3Shifted(), 3f);
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+            MoteMaker.TryThrowMicroSparks(base.Position.ToVector3Shifted());
+        }
+
+        protected override void Impact(Thing hitThing)
+        {
+            base.Impact(hitThing);
+            if (hitThing != null)
+            {
+                hitThing.TryIgnite(0.3f);
+                GenSpawn.Spawn(ThingDef.Named("Puddle_Fuel"), base.Position);
+                FireUtility.TryStartFireIn(base.Position, 0.2f);
+            }
+            else
+            {
+                GenSpawn.Spawn(ThingDef.Named("FlareDeployed"), this.Position);
+            }
+            MoteMaker.ThrowFlash(base.Position, "ShotFlash", 6f);
+            MoteMaker.TryThrowMicroSparks(base.Position.ToVector3Shifted());
+        }
+    }
+}
